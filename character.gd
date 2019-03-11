3cc6c97f379a15069d239fb984f1fd192bcd7ef2
Petya123456 <47749818+Petya123456@users.noreply.github.com>
2019-03-11 17:51:59 +0200

    Add files via upload

---
+++ character.gd
@@ -0,0 +1,18 @@
+extends Sprite
+var health = 100
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button2_pressed(extra_arg_0):
+	pass # Replace with function body.
